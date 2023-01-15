f1a6f9d2c83232b5959395bec1a54a27a2850cb3
Prajakta Pandurang Masal <98607075+Prajakta2011@users.noreply.github.com>
2023-01-15 23:21:58 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/server-image.jpg";
 
-const imageAltText = "woman holding laptop standing by server room with glass wall";
+const imageAltText = "Engineering is a future";
 
 const Home = ({ name, title }) => {
   return (
