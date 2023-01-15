5e7a2dec4c4ab791e052fc0093c340b93d0c9cd4
Prajakta Pandurang Masal <98607075+Prajakta2011@users.noreply.github.com>
2023-01-15 23:18:25 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,7 +21,7 @@ import React from "react";
  */
 import image from "../images/design-desk.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Computer Engineer";
 
 /**
  * Project list
@@ -31,28 +31,21 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Road Accident Prediction Model Using Machine Learning",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Frequency and density of Road Accident is predicted using Machine Learning",
+    
   },
   {
-    title: "Web Development for Beginners",
+    title: "Website for Art Gallery Management",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Website for Art Gallery Management is created using html, css, js", 
+    
   },
   {
-    title: "My Resume Site",
+    title: "Covid data analysis",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Database having covid data is mined, analyzed and visualized",
   },
 ];
 
